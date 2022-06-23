8c5b5afc7f8e01cd8ea189ab0dc00eff9da16d39
sac20 <sagudelo20@gmail.com>
2022-06-23 15:19:35 +0200

    conector

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import connection from "./connection";
+
+function App() {
+  return (
+    <div className="App">
+      {connection
+      }
+    </div>
+  );
+}
+
+export default App;
