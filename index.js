8414a5673114fe7ff2b0e746c89e06791fd2d1f1
Osman Jimenez <odjimenezg@unal.edu.co>
2022-07-01 17:38:20 -0500

    changed everything to HTML + JS pure

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom'
-import Counter from './Counter';
-
-ReactDom.render(<Counter/>, document.getElementById("app"));
---
+++ sparql.js
@@ -0,0 +1,12 @@
+
+SPARQL = function(o) {
+    this.query = function(q) {
+      return $.ajax({
+        url: o.endpoint,
+        accepts: {json: "application/sparql-results+json"},
+        data: {query: q, apikey: o.apikey},
+        dataType: "json"
+      });
+    };
+  };
+  
